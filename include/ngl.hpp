39d485cad29033185e4e760317fc9f5f2e24e3a0
Antoni <85031661+yak6@users.noreply.github.com>
2024-09-29 21:18:47 +0200

    move to include

--- include/ngl.hpp
+++ include/ngl.hpp
@@ -1,16 +1,18 @@
-#ifndef NGL_H
-#define NGL_H
+#ifndef NGL_HPP
+#define NGL_HPP
 
 #include <iostream>
 #include <string>
 #include <curl/curl.h>
 #include <time.h>
+#include <list>
+
 
 void send(std::string target, std::string question, std::string deviceId, std::string useragent);
 void spam(std::string target, std::list<std::string> questions, const std::string useragents, const int iterations, const std::string method, bool log);
 std::string fast_useragent();
-std::string random_string(int size); // can be used for random messages
+std::string random_string(int size); 
 std::string random_deviceId(); 
 std::string random_useragent(const std::string &filename);
 
-#endif // NGL_H
+#endif // NGL_HPP
